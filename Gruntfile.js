9ab606fad5f06ce6215c33aadb8092d76d1e0cc2
Chris Rebert <code@chrisrebert.com>
2015-12-22 14:40:27 -0700

    Remove grunt-contrib-internal dev dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,7 +61,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-internal');
 
   grunt.registerTask('mkdir', grunt.file.mkdir);
   grunt.registerTask('test', [
